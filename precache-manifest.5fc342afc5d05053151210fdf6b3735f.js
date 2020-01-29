14053fd50ae31cd12880d92e0917389d848d810d
David Ko <david.holy.ko@gmail.com>
2020-01-28 19:38:50 -0500

    Updates

---
+++ precache-manifest.5fc342afc5d05053151210fdf6b3735f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f61fb339368e5fa1caf522233d0f397",
+    "url": "/webpack-boilerplate/index.html"
+  },
+  {
+    "revision": "07927a4e0c7530f9f650",
+    "url": "/webpack-boilerplate/static/css/main.086696d8.chunk.css"
+  },
+  {
+    "revision": "ade7487c0dd0c91e72b8",
+    "url": "/webpack-boilerplate/static/js/2.3f712d60.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/webpack-boilerplate/static/js/2.3f712d60.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07927a4e0c7530f9f650",
+    "url": "/webpack-boilerplate/static/js/main.9d31f157.chunk.js"
+  },
+  {
+    "revision": "360ba9d65b1d634d1c8b",
+    "url": "/webpack-boilerplate/static/js/runtime-main.09aea516.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webpack-boilerplate/static/media/logo.5d5d9eef.svg"
+  }
+]);
